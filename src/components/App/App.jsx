323fe855bf4d50c04a915e53e948518867a341a3
Alona Zabolotskykh <alenka.polishchuk@gmail.com>
2024-08-15 13:47:06 +0300

    buttons+state

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,47 @@
 import { useState } from "react";
+import Description from "../Description/Description";
 
 import "./App.css";
 
+function Options({ children, updateFeedback }) {
+  return (
+    <>
+      <button onClick={updateFeedback}>{children}</button>
+    </>
+  );
+}
+
 export default function App() {
-  return <h1>Hello</h1>;
+  const [clicks, setClicks] = useState({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  });
+
+  const updateFeedback = (feedbackType) => {
+    setClicks((prevClicks) => ({
+      ...prevClicks,
+      [feedbackType]: prevClicks[feedbackType] + 1,
+    }));
+  };
+
+  const resetFeedback = () => {
+    setClicks({
+      good: 0,
+      neutral: 0,
+      bad: 0,
+    });
+  };
+
+  return (
+    <>
+      <Description />
+      <Options updateFeedback={() => updateFeedback("good")}> {"Good"}</Options>
+      <Options updateFeedback={() => updateFeedback("neutral")}>
+        {"Neutral"}
+      </Options>
+      <Options updateFeedback={() => updateFeedback("bad")}>{"Bad"} </Options>
+      <Options updateFeedback={resetFeedback}>{"Reset"} </Options>
+    </>
+  );
 }
