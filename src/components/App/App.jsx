3d5be494b7a6486509f27db32b5cbce97d4cd4cc
Alona Zabolotskykh <alenka.polishchuk@gmail.com>
2024-08-16 17:22:07 +0300

    task-6

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Description from "../Description/Description";
 import Options from "../Options/Options";
 import Feedback from "../Feedback/Feedback";
@@ -6,10 +6,13 @@ import Notification from "../Notification/Notification";
 import "./App.css";
 
 export default function App() {
-  const [clicks, setClicks] = useState({
-    good: 0,
-    neutral: 0,
-    bad: 0,
+  const [clicks, setClicks] = useState(() => {
+    const savedClicks = window.localStorage.getItem("all-clicks");
+
+    if (savedClicks !== null) {
+      return JSON.parse(savedClicks);
+    }
+    return { good: 0, neutral: 0, bad: 0 };
   });
 
   const updateFeedback = (feedbackType) => {
@@ -18,6 +21,10 @@ export default function App() {
       [feedbackType]: prevClicks[feedbackType] + 1,
     }));
   };
+
+  useEffect(() => {
+    window.localStorage.setItem("all-clicks", JSON.stringify(clicks));
+  });
 
   const resetFeedback = () => {
     setClicks({
